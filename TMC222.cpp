96deee7b85478ee79914ce1b6be1511afabe3c97
battosai30 <batto@hotmail.fr>
2014-04-04 11:37:23 +0200

    Upload

---
+++ TMC222.cpp
@@ -0,0 +1,246 @@
+#include "TMC222.h"
+ 
+TMC222::TMC222(byte Adress) {
+
+  _Adress=Adress;
+  Wire.begin();
+  
+}
+
+TMC222::~TMC222() {
+
+}
+
+int TMC222::GetActualPosition() {
+
+  GetFullStatus2();
+
+  return FullStatus_2.ActPos ;
+
+}
+
+long TMC222::GetTargetPosition() {
+
+  GetFullStatus2();
+
+  return FullStatus_2.TagPos ;
+
+}
+
+long TMC222::GetSecurePosition() {
+
+  GetFullStatus2();
+
+  return FullStatus_2.SecPos ;
+
+}
+
+void TMC222::SetTargetPosition(long Position) {
+
+  Wire.beginTransmission(_Adress);
+  Wire.write(C_SetPosition);
+  Wire.write(0xFF);
+  Wire.write(0xFF);
+  Wire.write((Position>>8) & 0xFF);
+  Wire.write(Position & 0xFF);
+  Wire.endTransmission();
+
+}
+
+void TMC222::RunInit() {
+
+  Command(C_RunInit);
+
+}
+
+void TMC222::HardStop(){
+
+  Command(C_HardStop);
+
+}
+
+void TMC222::SoftStop(){
+
+  Command(C_SoftStop);
+
+}
+
+byte TMC222::SoftMove(long Steps, byte ResetPosNeeded){
+
+  while(GetState()!=Stopped) {
+    if (_State==Hard_Stopped) return 0;
+    }
+  
+  if(ResetPosNeeded) {
+    ResetPosition();
+    SetTargetPosition(Steps);
+    ResetPosition();
+  } else {
+    SetTargetPosition(GetActualPosition()+Steps);
+  }
+  
+  return 1;
+
+}
+
+byte TMC222::HardMove(long Steps) {
+ 
+  HardStop();
+  GetFullStatus1();
+  ResetPosition();
+  SetTargetPosition(Steps);
+   //ResetPosition();
+  
+  
+}
+
+
+byte TMC222::GetState() {
+  GetFullStatus1();
+  _State=FullStatus_1.Motion;
+  return _State;
+}
+
+void TMC222::ResetPosition(){
+
+  Command(C_ResetPosition);
+
+}
+
+void TMC222::GotoSecurePosition(){
+
+  Command(C_GotoSecurePosition);
+
+}
+
+void TMC222::SetSecurePosition(long SecPos){
+  
+  _SecPos=SecPos;
+  
+  SetMotorParam(_Irun,_Ihold,_Vmax,_Vmin, SecPos,_Shaft,_Acc,_AccShape,_StepMode);
+
+}
+
+
+
+void TMC222::SetMotorParam(byte Irun, byte Ihold, byte Vmax, byte Vmin, unsigned int SecPos, byte Shaft,  byte Acc, byte AccShape, byte StepMode) {
+
+  _Irun =Irun;
+  _Ihold=Ihold;
+  _Vmax=Vmax;
+  _Vmin=Vmin;
+  _SecPos=SecPos;
+  _Shaft=Shaft;
+  _Acc=Acc;
+  _AccShape=AccShape;
+  _StepMode=StepMode;
+  
+  Wire.beginTransmission(_Adress);
+  Wire.write(C_SetMotorParam);
+  Wire.write(0xFF);
+  Wire.write(0xFF);
+  Wire.write(((Irun & 0xF)<<4) | (Ihold & 0xF));
+  Wire.write(((Vmax & 0xF)<<4) | (Vmin & 0xF));
+  Wire.write(((SecPos&0xE0)<<5) | ((Shaft&1)<<4) | (Acc & 0xF));
+  Wire.write(SecPos & 0xFF);
+  Wire.write(((AccShape & 1) <<4) | ((StepMode & 0x3)<<2));
+  Wire.endTransmission();
+
+}
+
+void TMC222::SetOTPParam(byte OTPA, byte PBits) {
+
+  Wire.beginTransmission(_Adress);
+  Wire.write(0xFF);
+  Wire.write(0xFF);
+  Wire.write( 0xF8 | (OTPA & 0x7));
+  Wire.write( PBits & 0xFF );
+  Wire.endTransmission();
+
+}
+
+void TMC222::GetFullStatus1() {
+
+  byte Buffer;
+
+  Command(C_GetFullStatus1);
+  
+  Wire.requestFrom(_Adress, 6);    
+
+  FullStatus_1.Address = Wire.read();
+ 
+  Buffer=Wire.read();
+  
+  FullStatus_1.Irun = GetBytePart(Buffer,4,4);
+  FullStatus_1.Ihold = GetBytePart(Buffer,0,4);
+  Buffer=Wire.read();
+  
+  FullStatus_1.Vmax = GetBytePart(Buffer,4,4);
+  FullStatus_1.Vmin = GetBytePart(Buffer,0,4);
+  Buffer=Wire.read();
+  
+  FullStatus_1.AccShape = GetBytePart(Buffer,7,1);
+  FullStatus_1.StepMode = GetBytePart(Buffer,5,2);
+  FullStatus_1.Shaft = GetBytePart(Buffer,4,1);
+  FullStatus_1.ACC = GetBytePart(Buffer,0,4);
+  Buffer=Wire.read();
+  
+  FullStatus_1.VddReset = GetBytePart(Buffer,7,1);
+  FullStatus_1.StepLoss = GetBytePart(Buffer,6,1);
+  FullStatus_1.EIDef= GetBytePart(Buffer,5,1);
+  FullStatus_1.UV2 = GetBytePart(Buffer,4,1);
+  FullStatus_1.TSD = GetBytePart(Buffer,3,1);
+  FullStatus_1.TW = GetBytePart(Buffer,2,1);
+  FullStatus_1.Tinfo = GetBytePart(Buffer,0,2);
+  Buffer=Wire.read();
+  
+  FullStatus_1.Motion = GetBytePart(Buffer,5,3);
+  FullStatus_1.ESW = GetBytePart(Buffer,4,1);
+  FullStatus_1.OVC1= GetBytePart(Buffer,3,1);
+  FullStatus_1.OVC2 = GetBytePart(Buffer,2,1);
+  FullStatus_1.CPFail = GetBytePart(Buffer,0,1);
+  
+  while(Wire.available()) Wire.read(); // Flush    
+}
+
+void TMC222::GetFullStatus2() {
+  
+  Command(C_GetFullStatus2);
+ 
+  Wire.requestFrom(_Adress, 6);    
+
+  FullStatus_2.Address = Wire.read();
+  
+  FullStatus_2.ActPos = Wire.read()<<8;
+  FullStatus_2.ActPos |= Wire.read();
+  FullStatus_2.TagPos = Wire.read()<<8;
+  FullStatus_2.TagPos |= Wire.read();
+  FullStatus_2.SecPos = Wire.read();
+  FullStatus_2.SecPos |= (Wire.read()&0x07)<<8;
+  
+  while(Wire.available()) Wire.read(); // Flush    
+  
+}
+
+void TMC222::Command(byte Reg) {
+
+  Wire.beginTransmission(_Adress);
+  Wire.write(Reg);
+  Wire.endTransmission();
+
+}
+
+byte TMC222::GetBytePart(byte Source, byte StartPos, byte BitQuantity) {
+  
+  byte Extracted=0;
+  
+  Source = ((Source & 0xFF) >> StartPos); 
+  
+  for(byte i=0;i<BitQuantity;i++) {
+    Extracted |= Source & (1<<i);
+  }
+
+return Extracted;
+  
+  
+}
---
+++ TMC222.h
@@ -0,0 +1,164 @@
+/* TMC222 library version 1.0
+
+Writed by B@tto 
+Contact : batto@hotmail.fr
+
+
+  MCP3424.h - ADC 18 bits i2c library for Wiring & Arduino
+  Copyright (c) 2012 Yann LEFEBVRE.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef TMC22_H
+#define TMC22_H
+
+#include <Wire.h>
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+/* Current constants definition */
+
+#define I_59mA 0x0
+#define I_71mA 0x1
+#define I_84mA 0x2
+#define I_100mA 0x3
+#define I_119mA 0x4
+#define I_141mA 0x5
+#define I_168mA 0x6
+#define I_200mA 0x7
+#define I_238mA 0x8
+#define I_283mA 0x9
+#define I_336mA 0xA
+#define I_400mA 0xB
+#define I_476mA 0xC
+#define I_566mA 0xD
+#define I_673mA 0xE
+#define I_800mA 0xF
+
+/* Shaft bytes */
+#define Shaft_Clockwise 0x0
+#define Shaft_AntiClockwise 0x1
+
+/* StepMode bytes */
+#define StepMode_1_2 0x0
+#define StepMode_1_4 0x1
+#define StepMode_1_8 0x2
+#define StepMode_1_16 0x3
+
+/* Status bytes */
+#define Stopped 0x0
+#define Acc_Pos_C 0x1
+#define Acc_Neg_C 0x2
+#define Moving_Vmax_C 0x3
+#define Hard_Stopped 0x4
+#define Acc_Pos_AC 0x5
+#define Acc_Neg_AC 0x6
+#define Moving_Vmax_AC 0x7
+
+
+/* Command bytes */
+#define C_GetFullStatus1 0x81
+#define C_GetFullStatus2 0xFC
+#define C_GetOTPParam 0x82
+#define C_GotoSecurePosition 0x84
+#define C_HardStop 0x85
+#define C_ResetPosition 0x86
+#define C_ResetToDefault 0x87
+#define C_RunInit 0x88
+#define C_SetMotorParam 0x89
+#define C_SetOTPParam 0x90
+#define C_SetPosition 0x8B
+#define C_SoftStop 0x8F
+
+class TMC222 {
+
+public:
+
+ TMC222(byte Adress);
+ ~TMC222();
+ 
+ void GetFullStatus1();
+ void GetFullStatus2();
+ byte GetState() ;
+ int GetActualPosition();
+ long GetTargetPosition();
+ long GetSecurePosition();
+ 
+ byte SoftMove(long Steps, byte ResetPosNeeded);
+ void SoftStop();
+ void HardStop();
+ byte HardMove(long Steps);
+ void GotoSecurePosition();
+ void RunInit();
+
+ 
+ void SetSecurePosition(long SecPos);
+ void SetTargetPosition(long Position);
+ void ResetPosition();
+ 
+ void SetOTPParam(byte OTPA, byte PBits);
+ void SetMotorParam(byte Irun, byte Ihold, byte Vmax, byte Vmin, unsigned int SecPos, byte Shaft,  byte Acc, byte AccShape, byte StepMode);
+ 
+   struct { 
+  unsigned char SlaveAddress; 
+  unsigned char Address; 
+  unsigned char Irun; 
+  unsigned char Ihold; 
+  unsigned char Vmax; 
+  unsigned char Vmin; 
+  unsigned char AccShape; 
+  unsigned char StepMode; 
+  unsigned char Shaft; 
+  unsigned char ACC; 
+  unsigned char VddReset; 
+  unsigned char StepLoss; 
+  unsigned char EIDef; 
+  unsigned char UV2; 
+  unsigned char TSD; 
+  unsigned char TW; 
+  unsigned char Tinfo; 
+  unsigned char Motion; 
+  unsigned char ESW;
+  unsigned char OVC1;
+  unsigned char OVC2,;
+  unsigned char CPFail;
+} 
+ FullStatus_1 ;
+ 
+ struct { 
+  byte SlaveAddress; 
+  byte Address; 
+  int ActPos; 
+  int TagPos; 
+  int SecPos; 
+} 
+FullStatus_2;
+
+ private:
+ 
+ byte _Irun,_Ihold,_Vmax,_Vmin, _SecPos,_Shaft,_Acc,_AccShape,_StepMode,_State;
+ int _Adress;
+ void Command(byte Reg);
+ byte GetBytePart(byte Source, byte StartPos, byte BitQuantity);
+ 
+ };
+
+
+ #endif
